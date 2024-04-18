d16786c1295cc7af9a8d9d2461b113097aa38349
Paula Masutier <paulamasutier@gmail.com>
2024-04-18 17:29:29 +0200

    Test: shopping cart end to end test

---
+++ src/main/java/org/example/rest_api/ShoppingCartController.java
@@ -0,0 +1,15 @@
+package org.example.rest_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("shopping-cart")
+public class ShoppingCartController {
+
+    @GetMapping
+    public String getShoppingCart() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/org/example/rest_api/ShoppingCartEndToEnd.java
@@ -0,0 +1,34 @@
+package org.example.rest_api;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ShoppingCartController.class)
+public class ShoppingCartEndToEnd {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Test
+    public void end_to_end_test() throws Exception {
+
+        JSONObject expectedShoppingCart = new JSONObject();
+        expectedShoppingCart.put("products", List.of());
+        expectedShoppingCart.put("discount", null);
+        expectedShoppingCart.put("total", "0 €");
+
+
+        mockMvc.perform(get("/shopping-cart"))
+            .andExpect(status().isOk())
+            .andExpect(content().json(expectedShoppingCart.toString()));
+    }
+}
